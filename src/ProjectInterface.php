0a41cce8670af91b29bbe1299ba9e9c0d6838ec2
OMAHM <hello@onemanandhismouse.com>
2022-09-22 11:06:14 +0100

    Initial Interfaces and utils

---
+++ src/ProjectInterface.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Maestro\Core;
+
+/**
+ * Interface for Maestro Project definition.
+ *
+ * IMPORTANT: Changes to this interface must be replicated to the matching
+ * interface within the MaestroHosting project.
+ */
+interface ProjectInterface {
+
+  /**
+   * Save the project.
+   */
+  public function save();
+
+  /**
+   * Add a site to the project.
+   *
+   * @param string $site_id
+   *   Site identifier.
+   * @param array $site_data
+   *   Site configuration data.
+   */
+  public function addSite(string $site_id, array $site_data);
+
+  /**
+   * Update a site within the project.
+   *
+   * @param string $site_id
+   *   Site identifier.
+   * @param array $site_data
+   *   Site configuration data.
+   */
+  public function updateSite(string $site_id, array $site_data);
+
+  /**
+   * Remove a site from the project.
+   *
+   * @param string $site_id
+   *   Site identifier.
+   */
+  public function removeSite(string $site_id);
+
+  /**
+   * Project name.
+   *
+   * @return string|null
+   *   The project name.
+   */
+  public function name();
+
+  /**
+   * Project ID.
+   *
+   * @return string|null
+   *   The project ID.
+   */
+  public function id();
+
+  /**
+   * Project type.
+   *
+   * @return string|null
+   *   The project type.
+   */
+  public function type();
+
+  /**
+   * Sites for the project.
+   *
+   * @return array
+   *   Array of project sites.
+   */
+  public function sites();
+
+  /**
+   * Determine if a site exists in the Project.
+   *
+   * @return bool
+   *   True if site exists, otherwise false.
+   */
+  public function siteExists($site_id);
+
+}
