0c3b871537ed997d51a1f90fe00ed5e7ca0726d0
OMAHM <hello@onemanandhismouse.com>
2022-09-25 21:53:57 +0100

    Call underlying filesystem methods

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -5,37 +5,48 @@ namespace Maestro\Core;
 class Filesystem {
 
   protected $rootPath;
+  protected $fs;
 
   public function __construct($path) {
     $this->rootPath = $path;
+    $this->fs = new \Symfony\Component\Filesystem\Filesystem();
+
   }
 
   public function exists($path) {
-
+    return $this->fs->exists($this->fullPath($path));
   }
 
   public function read($path) {
-
+    return $this->fs->exists($this->fullPath($path));
   }
 
   public function write($path, $content) {
-
+      return $this->fs->dumpFile($this->fullPath($path), $content);
   }
 
   public function delete($path) {
-
+    return $this->fs->remove($this->fullPath($path));
   }
 
-  public function copy() {
-
+  public function copy($path, $destination) {
+    if ($this->isDir($path)) {
+      return $this->fs->mirror($this->fullPath($path), $this->fullPath($destination));
+    } else {
+      return $this->fs->copy($this->fullPath($path), $this->fullPath($destination));
+    }
   }
 
   public function createDirectory($path) {
-
+    return $this->fs->mkdir($this->fullPath($path));
   }
 
   public function link($source, $link) {
+    return $this->fs->symlink($this->fullPath($source), $this->fullPath($link));
+  }
 
+  public function isDir($path){
+    return is_dir($this->fullPath($path));
   }
 
   protected function fullPath($path) {
